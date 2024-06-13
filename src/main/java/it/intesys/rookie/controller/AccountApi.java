e965d8c5f4aaab129b7a5d23fd088d7e0098b231
feder <federicobosco06@gmail.com>
2024-06-13 16:31:40 +0200

    fesashaftyas

---
+++ src/main/java/it/intesys/rookie/controller/AccountApi.java
@@ -0,0 +1,39 @@
+package it.intesys.rookie.controller;
+
+import it.intesys.rookie.dto.AccountDTO;
+import it.intesys.rookie.service.AccountService;
+import it.intesys.rookie.service.NotFound;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+public class AccountApi {
+    public static final String API_ACCOUNT = "/api/account";
+    public static final String API_ACCOUNT_ID = API_ACCOUNT + "/{id}";
+    private final AccountService accountService;
+
+    public AccountApi(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @PostMapping(API_ACCOUNT)
+    AccountDTO createAccount(@RequestBody AccountDTO account){
+        return accountService.createAccount(account);
+    }
+    @GetMapping(API_ACCOUNT_ID)
+    ResponseEntity<AccountDTO> getAccount(@PathVariable Long id){
+        try {
+            AccountDTO account = accountService.getAccount(id);
+            return ResponseEntity.ok(account);
+        } catch (NotFound e){
+            return ResponseEntity.notFound().header("x-rookie-error", e.getMessage()).build();
+        }
+    }
+    @PutMapping
+    public AccountDTO updateAccount(@PathVariable Long id, @RequestBody AccountDTO accountDTO){
+        return accountService.updateAccount(id, accountDTO);
+    }
+}
+
