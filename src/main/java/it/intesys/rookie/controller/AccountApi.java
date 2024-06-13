1e4822f6547067d194c5eacb394c9ddb37dfc64d
PC_MARCONI <19842@studenti.marconiverona.edu.it>
2024-06-13 16:32:28 +0200

    account gestione

---
+++ src/main/java/it/intesys/rookie/controller/AccountApi.java
@@ -0,0 +1,36 @@
+package it.intesys.rookie.controller;
+
+import it.intesys.rookie.dto.AccountDTO;
+import it.intesys.rookie.service.AccountService;
+import it.intesys.rookie.service.NotFound;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class AccountApi {
+    public static final String API_ACCOUNT = "/api/account";
+    public static final String API_ACCOUNT_ID = API_ACCOUNT + "/{id}";
+    private final AccountService accountService;
+
+    public AccountApi(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @PostMapping("/api/account")
+
+    AccountDTO createAccount(@RequestBody AccountDTO account){
+        return  accountService.createAccount (account);
+    }
+    @GetMapping(API_ACCOUNT_ID)
+    ResponseEntity <AccountDTO> getAccount(@PathVariable Long id){
+        try{
+            AccountDTO account = accountService.getAccount(id);
+            return ResponseEntity.ok(account);
+        }catch(NotFound e){
+            return ResponseEntity.notFound().header("X-rookie-header",e.getMessage()).build();
+        }
+
+
+    }
+
+}
---
+++ src/main/java/it/intesys/rookie/domain/Account.java
@@ -0,0 +1,69 @@
+package it.intesys.rookie.domain;
+
+import java.time.Instant;
+
+public class Account {
+    private Long id;
+    private Instant dateCreated;
+    private Instant dateModified;
+    private String alias;
+    private String name;
+    private String surname;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String email;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Instant dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Instant getDateModified() {
+        return dateModified;
+    }
+
+    public void setDateModified(Instant dateModified) {
+        this.dateModified = dateModified;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
