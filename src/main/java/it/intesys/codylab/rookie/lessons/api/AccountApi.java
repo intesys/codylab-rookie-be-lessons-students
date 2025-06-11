95271cd7bf417ac71c99e061c89084f3c382dce2
unknown <damipisci434@gmail.com>
2025-06-11 12:40:06 +0200

    controller + service

---
+++ src/main/java/it/intesys/codylab/rookie/lessons/api/AccountApi.java
@@ -0,0 +1,19 @@
+package it.intesys.codylab.rookie.lessons.api;
+
+import it.intesys.codylab.rookie.lessons.dto.AccountDto;
+import it.intesys.codylab.rookie.lessons.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountApi {
+    @Autowired
+    AccountService accountService;
+
+    @PostMapping("/api/account")
+    void createAccount(@RequestBody AccountDto accountDto){
+        accountService.createAccount(accountDto);
+    }
+}
