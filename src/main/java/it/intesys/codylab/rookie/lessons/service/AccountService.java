d619be8f07fd6685b4b8ad24b575ed8bd2b542a7
unknown <jasminderkaur2910@gmail.com>
2025-06-11 12:41:28 +0200

    controller service

---
+++ src/main/java/it/intesys/codylab/rookie/lessons/service/AccountService.java
@@ -0,0 +1,11 @@
+package it.intesys.codylab.rookie.lessons.service;
+
+import it.intesys.codylab.rookie.lessons.dto.AccountDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService {
+
+    public void creatAccount(AccountDto accountDto) {
+    }
+}
