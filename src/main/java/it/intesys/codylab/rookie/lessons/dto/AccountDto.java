abddf28d10bca74ea856e2de793a88c4fb1129ae
unknown <zeno.spadi@gmail.com>
2025-06-11 12:40:52 +0200

    controller + service

---
+++ src/main/java/it/intesys/codylab/rookie/lessons/dto/AccountDto.java
@@ -0,0 +1,72 @@
+package it.intesys.codylab.rookie.lessons.dto;
+
+import java.time.Instant;
+
+public class AccountDto {
+     private long id;
+
+     private String alias;
+     private String name;
+     private String surname;
+     private String email;
+     private Instant dateCreated;
+     private Instant dateModified;
+
+     public long getId() {
+          return id;
+     }
+
+     public void setId(long id) {
+          this.id = id;
+     }
+
+     public String getAlias() {
+          return alias;
+     }
+
+     public void setAlias(String alias) {
+          this.alias = alias;
+     }
+
+     public String getName() {
+          return name;
+     }
+
+     public void setName(String name) {
+          this.name = name;
+     }
+
+     public String getSurname() {
+          return surname;
+     }
+
+     public void setSurname(String surname) {
+          this.surname = surname;
+     }
+
+     public String getEmail() {
+          return email;
+     }
+
+     public void setEmail(String email) {
+          this.email = email;
+     }
+
+     public Instant getDateCreated() {
+          return dateCreated;
+     }
+
+     public void setDateCreated(Instant dateCreated) {
+          this.dateCreated = dateCreated;
+     }
+
+     public Instant getDateModified() {
+          return dateModified;
+     }
+
+     public void setDateModified(Instant dateModified) {
+          this.dateModified = dateModified;
+     }
+}
+
+
---
+++ src/main/java/it/intesys/codylab/rookie/lessons/service/AccountService.java
@@ -0,0 +1,8 @@
+package it.intesys.codylab.rookie.lessons.service;
+
+import it.intesys.codylab.rookie.lessons.dto.AccountDto;
+
+public class AccountService {
+    public void createAccount(AccountDto accountDto) {
+    }
+}
