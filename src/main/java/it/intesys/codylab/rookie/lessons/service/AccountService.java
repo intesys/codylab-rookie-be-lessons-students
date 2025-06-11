42b7124adbbf69205883756fb8c09e5074f1df47
fede scarcella <federico.scarcella2006@gmail.com>
2025-06-11 12:40:16 +0200

    controller + service

---
+++ src/main/java/it/intesys/codylab/rookie/lessons/service/AccountService.java
@@ -0,0 +1,13 @@
+package it.intesys.codylab.rookie.lessons.service;
+
+import it.intesys.codylab.rookie.lessons.dto.AccountDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService {
+
+    public void createAccaunt(AccountDto accountDto){
+
+    }
+}
